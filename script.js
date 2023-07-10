077d70bdfa8c60aaf292c8322b5826d74548e0f5
Klaivert <Klaivert.ytb@gmail.com>
2023-07-10 20:43:05 +0200

    V2

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ $(".option").click(function(){
     $(this).addClass("active");
     
  });
+
+ document.addEventListener("DOMContentLoaded", function() {
+    var shapes = document.querySelectorAll(".shape");
+    
+    shapes.forEach(function(shape) {
+        shape.addEventListener("mouseover", function() {
+            this.style.backgroundColor = "#ff0000";
+        });
+        
+        shape.addEventListener("mouseout", function() {
+            this.style.backgroundColor = "#ffffff";
+        });
+    });
+});
+
