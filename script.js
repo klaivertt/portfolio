a42d3da2fb07f99b856bc415230c231fe704c18b
Klaivert <anteionstuio@gmail.com>
2023-10-21 19:29:29 +0200

    v2.0

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-// Sélectionnez toutes les images de la galerie
-const images = document.querySelectorAll('.image-gallery img');
-
-// Ajoutez un gestionnaire d'événement pour chaque image
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        // Réduisez la taille de toutes les images (sauf celle cliquée)
-        images.forEach(img => {
-            if (img !== image) {
-                img.style.transform = 'scale(1)';
-            }
-        });
-
-        // Agrandissez l'image cliquée
-        image.style.transform = 'scale(2)';
-    });
-});
+$(".option").click(function(){
+    $(".option").removeClass("active");
+    $(this).addClass("active");
+    
+ });
